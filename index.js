ad1aa5d2bae0fbfb7ffda2c016ef0432b4c37c99
mrwerner392 <mrwerner392@gmail.com>
2021-04-29 14:07:39 -0400

    Define and build newGrid.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // handle one single evolution. call repeatedly to get future evolutions
 const evolve = grid => {
   // define and build new grid to hold the new values
+  const newGrid = []
+  for (let i = 0; i < 10; i++) {
+    newGrid.push(new Array(10))
+  }
   // iterate through each element of nxn the input grid
     // first check value, then check which rule applies for that value (using surrounding input grid values)
     // determine value for new grid
