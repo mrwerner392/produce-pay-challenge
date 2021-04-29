984a3b5ea3942913844cbf5b8477c36a9701c43e
mrwerner392 <mrwerner392@gmail.com>
2021-04-29 14:09:35 -0400

    Update logic for any size input grid.

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 const evolve = grid => {
   // define and build new grid to hold the new values
   const newGrid = []
-  for (let i = 0; i < 10; i++) {
-    newGrid.push(new Array(10))
+  for (let i = 0; i < grid.length; i++) {
+    newGrid.push(new Array(grid.length))
   }
   // iterate through each element of nxn the input grid
     // first check value, then check which rule applies for that value (using surrounding input grid values)
