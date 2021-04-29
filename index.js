28e4e3c3d0860618b6059046ed6e97e76a20ad7d
mrwerner392 <mrwerner392@gmail.com>
2021-04-29 14:03:57 -0400

    Define evolve function and outline steps.

---
+++ index.js
@@ -0,0 +1,11 @@
+// handle one single evolution. call repeatedly to get future evolutions
+const evolve = grid => {
+  // define and build new grid to hold the new values
+  // iterate through each element of nxn the input grid
+    // first check value, then check which rule applies for that value (using surrounding input grid values)
+    // determine value for new grid
+    // update appropriate element in new grid
+  // return new grid
+}
+
+module.exports = evolve
