421659256bbdfd7b1371ea0b99039635025223ba
mrwerner392 <mrwerner392@gmail.com>
2021-04-29 14:27:12 -0400

    Wireframe for nested for loop through input grid.

--- index.js
+++ index.js
@@ -5,10 +5,28 @@ const evolve = grid => {
   for (let i = 0; i < grid.length; i++) {
     newGrid.push(new Array(grid.length))
   }
+
   // iterate through each element of nxn the input grid
     // first check value, then check which rule applies for that value (using surrounding input grid values)
     // determine value for new grid
     // update appropriate element in new grid
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid.length; j++) {
+      const currentVal = grid[i][j]
+      const neighbors = []
+      let newVal
+      if (!currentVal) {
+
+      } else if (currentVal === 1) {
+
+      } else if (currentVal === 2) {
+
+      } else if (currentVal === 3) {
+
+      }
+    }
+  }
+
   // return new grid
 }
 
